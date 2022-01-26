bb6ec8fe527386e1f2db55ad7d269e7f2f2fb124
Peyton Paul <peyton.z.paul@gmail.com>
2022-01-25 23:39:23 -0500

    CheckPoints and Respawn

---
+++ The Black Cat/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public SpriteRenderer theSR;
+    public Sprite cpHit, cpNotHit;
+
+    void Start()
+    {
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GameManager.instance.DeactiveCheckPoints();
+            theSR.sprite = cpHit;
+
+            GameManager.instance.SetSpawnPoint(transform.position);
+        }
+    }
+
+    public void ResetCheckPoint()
+    {
+        theSR.sprite = cpNotHit;
+    }
+}
