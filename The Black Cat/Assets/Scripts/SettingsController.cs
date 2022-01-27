0a7b9fcf0284e4e286ad4227e2f82253ddbbdb96
Peyton Paul <peyton.z.paul@gmail.com>
2022-01-27 06:28:43 -0500

    Loading Screen and Menu Additions

--- The Black Cat/Assets/Scripts/SettingsController.cs
+++ The Black Cat/Assets/Scripts/SettingsController.cs
@@ -1,16 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsController : MonoBehaviour
 {
+    [Header("Fullscreen Toggle Variables")]
+    public Toggle fullScreenToggle;
+    [HideInInspector] public bool isFullScreenOn;
+    [HideInInspector] public int screenInt;
+
     void Start()
     {
-
+        //FullScreen Toggle Save
+        screenInt = PlayerPrefs.GetInt("FullScreenState");
+        if (screenInt == 1)
+        {
+            fullScreenToggle.isOn = false;
+        }
+        else
+        {
+            isFullScreenOn = true;
+            fullScreenToggle.isOn = true;
+        }
     }
 
     void Update()
     {
 
+    }
+
+    public void FullScreenMode(bool isFullScreenOn)
+    {
+        Screen.fullScreen = isFullScreenOn;
+
+        if (isFullScreenOn == false)
+        {
+            isFullScreenOn = false;
+            PlayerPrefs.SetInt("FullScreenState", 1);
+        }
+        else
+        {
+            isFullScreenOn = true;
+            PlayerPrefs.SetInt("FullScreenState", 0);
+            Debug.Log("Changing to FullScreen");
+        }
     }
 }
