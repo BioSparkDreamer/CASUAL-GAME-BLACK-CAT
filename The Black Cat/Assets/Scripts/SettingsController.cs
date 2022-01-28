d1db17bdd767bc3144ac5a2f2ff6e30ee4e36048
Peyton Paul <peyton.z.paul@gmail.com>
2022-01-28 09:22:49 -0500

    Vsync and FPS toggle

--- The Black Cat/Assets/Scripts/SettingsController.cs
+++ The Black Cat/Assets/Scripts/SettingsController.cs
@@ -9,8 +9,17 @@ public class SettingsController : MonoBehaviour
 {
     [Header("Fullscreen Toggle Variables")]
     public Toggle fullScreenToggle;
-    [HideInInspector] public bool isFullScreenOn;
     [HideInInspector] public int screenInt;
+
+    [Header("FPS Counter Variables")]
+    public Toggle fpsToggle;
+    public TMP_Text fpsText;
+    public GameObject fpsCounterObject;
+    [HideInInspector] public int fpsInt;
+
+    [Header("Vsync Toggle Variables")]
+    public Toggle syncToggle;
+    [HideInInspector] public int syncInt;
 
     void Start()
     {
@@ -22,14 +31,41 @@ public class SettingsController : MonoBehaviour
         }
         else
         {
-            isFullScreenOn = true;
             fullScreenToggle.isOn = true;
+        }
+
+        //FPS Counter Toggle Save
+        fpsInt = PlayerPrefs.GetInt("FpsToggleState");
+        if (fpsInt == 1)
+        {
+            fpsToggle.isOn = false;
+            fpsCounterObject.SetActive(false);
+        }
+        else
+        {
+            fpsToggle.isOn = true;
+            fpsCounterObject.SetActive(true);
+        }
+
+        //Vsync Toggle Save
+        syncInt = PlayerPrefs.GetInt("SyncToggleState");
+        if (syncInt == 1)
+        {
+            syncToggle.isOn = false;
+            QualitySettings.vSyncCount = 0;
+        }
+        else
+        {
+            syncToggle.isOn = true;
+            QualitySettings.vSyncCount = 1;
         }
     }
 
     void Update()
     {
-
+        //Calculating FPS
+        float fps = 1 / Time.unscaledDeltaTime;
+        fpsText.text = "FPS: " + fps.ToString("F2");
     }
 
     public void FullScreenMode(bool isFullScreenOn)
@@ -46,6 +82,36 @@ public class SettingsController : MonoBehaviour
             isFullScreenOn = true;
             PlayerPrefs.SetInt("FullScreenState", 0);
             Debug.Log("Changing to FullScreen");
+        }
+    }
+
+    public void FpsToggleMode(bool isFpsOn)
+    {
+        if (isFpsOn == false)
+        {
+            PlayerPrefs.SetInt("FpsToggleState", 1);
+            fpsCounterObject.SetActive(false);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("FpsToggleState", 0);
+            fpsCounterObject.SetActive(true);
+            Debug.Log("FPS Counter is Off");
+        }
+    }
+
+    public void VsyncToggleMode(bool isSyncOn)
+    {
+        if (isSyncOn == false)
+        {
+            PlayerPrefs.SetInt("SyncToggleState", 1);
+            QualitySettings.vSyncCount = 0;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("SyncToggleState", 0);
+            QualitySettings.vSyncCount = 1;
+            Debug.Log("Vsync is On");
         }
     }
 }
